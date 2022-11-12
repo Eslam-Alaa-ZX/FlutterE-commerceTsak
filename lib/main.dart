75e35a27d5b359cc9afbbe450163dfa3685d673d
Eslam Alaa <zoro12zx@gmail.com>
2022-11-12 21:05:54 +0200

    Eslam Alaa-Start app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommarce_task/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home: const Login(),
     );
   }
 }
