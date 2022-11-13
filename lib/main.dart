2552e7414e940f006ed5f15adab248f50e0ba9de
Eslam Alaa <zoro12zx@gmail.com>
2022-11-13 14:24:31 +0200

    Eslam Alaa-Finish appbar for home page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommarce_task/pages/home_page.dart';
 import 'package:flutter_ecommarce_task/pages/login.dart';
 import 'package:flutter_ecommarce_task/pages/sign_up.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SignUp(),
+      home: const HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 76, 141, 95),
+          title: const Text("Home"),
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 24,
+                      child: Container(
+                          child: const Text(
+                            "8",
+                            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          padding: const EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(right: 12),
+                  child: Text("\$ 128"),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
