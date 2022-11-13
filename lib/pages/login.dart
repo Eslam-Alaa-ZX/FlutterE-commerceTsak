cb8cf4b85d6d1f1166898b01af63219afd7056e2
Eslam Alaa <zoro12zx@gmail.com>
2022-11-13 08:03:48 +0200

    Eslam Alaa-End Login page.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,55 +8,58 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 64,
-              ),
-              const MyCustomTextField(
-                inputType: TextInputType.emailAddress,
-                isPassword: false,
-                hint: "Enter Your Email : ",
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              const MyCustomTextField(
-                inputType: TextInputType.text,
-                isPassword: true,
-                hint: "Enter Your Password : ",
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              MyCBtn(
-                color: BTNgreen,
-                txt: "Sign in",
-                onClick: () {},
-              ),
-              const SizedBox(
-                height: 33,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Do not have an account? ",
-                      style: TextStyle(
-                        fontSize: 18,
-                      )),
-                  MyCTxtBtn(
-                    onClick: () {},
-                    color: Colors.black,
-                    txt: "Sign up",
-                  )
-                ],
-              ),
-            ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                const MyCustomTextField(
+                  inputType: TextInputType.emailAddress,
+                  isPassword: false,
+                  hint: "Enter Your Email : ",
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                const MyCustomTextField(
+                  inputType: TextInputType.text,
+                  isPassword: true,
+                  hint: "Enter Your Password : ",
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                MyCBtn(
+                  color: BTNgreen,
+                  txt: "Sign in",
+                  onClick: () {},
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Do not have an account? ",
+                        style: TextStyle(
+                          fontSize: 18,
+                        )),
+                    MyCTxtBtn(
+                      onClick: () {},
+                      color: Colors.black,
+                      txt: "Sign up",
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
