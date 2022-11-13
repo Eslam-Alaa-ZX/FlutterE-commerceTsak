68b1cd731061e973571c0857e72f1ba5eda90a3e
Eslam Alaa <zoro12zx@gmail.com>
2022-11-13 15:08:58 +0200

    Eslam Alaa-Drawer for home page.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,57 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  const UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/img/test.jpg"),
+                          fit: BoxFit.cover),
+                    ),
+                    accountName: Text("Eslam Alaa",
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                    accountEmail: Text("zoro12zx@gmail.com",
+                        style: TextStyle(
+                          color: Colors.black,
+                        )),
+                    currentAccountPictureSize: Size.square(99),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets/img/person.jpg")),
+                  ),
+                  ListTile(
+                      title: const Text("Home"),
+                      leading: const Icon(Icons.home),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("My products"),
+                      leading: const Icon(Icons.add_shopping_cart),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("About"),
+                      leading: const Icon(Icons.help_center),
+                      onTap: () {}),
+                  ListTile(
+                      title: const Text("Logout"),
+                      leading: const Icon(Icons.exit_to_app),
+                      onTap: () {}),
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 12),
+                child: const Text("Developed by Eslam Alaa © 2022",
+                    style: TextStyle(fontSize: 16)),
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 76, 141, 95),
           title: const Text("Home"),
@@ -20,7 +71,8 @@ class HomePage extends StatelessWidget {
                       child: Container(
                           child: const Text(
                             "8",
-                            style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                            style:
+                                TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
                           ),
                           padding: const EdgeInsets.all(5),
                           decoration: const BoxDecoration(
