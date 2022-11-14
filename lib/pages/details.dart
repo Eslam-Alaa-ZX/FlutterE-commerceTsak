2c11a09be1ddc3cede5358b68551fc7ae06a927d
Eslam Alaa <zoro12zx@gmail.com>
2022-11-14 11:04:26 +0200

    Eslam Alaa-Details page: item details.

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -49,91 +49,110 @@ class _DetailsState extends State<Details> {
               )
             ],
           ),
-          body: Column(
-            children: [
-              Image.asset("assets/img/2.webp"),
-              const SizedBox(
-                height: 11,
-              ),
-              const Text(
-                "\$ 12.99",
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                      padding: const EdgeInsets.all(4),
-                      child: const Text(
-                        "New",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 255, 129, 129),
-                        borderRadius: BorderRadius.circular(4),
-                      )),
-                  const SizedBox(
-                    width: 8,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset("assets/img/2.webp"),
+                const SizedBox(
+                  height: 11,
+                ),
+                const Text(
+                  "\$ 12.99",
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.all(4),
+                        child: const Text(
+                          "New",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 129, 129),
+                          borderRadius: BorderRadius.circular(4),
+                        )),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.star,
+                          size: 26,
+                          color: Color.fromARGB(255, 255, 191, 0),
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 26,
+                          color: Color.fromARGB(255, 255, 191, 0),
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 26,
+                          color: Color.fromARGB(255, 255, 191, 0),
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 26,
+                          color: Color.fromARGB(255, 255, 191, 0),
+                        ),
+                        Icon(
+                          Icons.star,
+                          size: 26,
+                          color: Color.fromARGB(255, 255, 191, 0),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 66,
+                    ),
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.edit_location,
+                          size: 26,
+                          color: Color.fromARGB(168, 3, 65, 27),
+                          // color: Color.fromARGB(255, 186, 30, 30),
+                        ),
+                        SizedBox(
+                          width: 3,
+                        ),
+                        Text(
+                          "Flower Shop",
+                          style: TextStyle(fontSize: 19),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Details : ",
+                    style: TextStyle(fontSize: 22),
+                    textAlign: TextAlign.start,
                   ),
-                  Row(
-                    children: const [
-                      Icon(
-                        Icons.star,
-                        size: 26,
-                        color: Color.fromARGB(255, 255, 191, 0),
-                      ),
-                      Icon(
-                        Icons.star,
-                        size: 26,
-                        color: Color.fromARGB(255, 255, 191, 0),
-                      ),
-                      Icon(
-                        Icons.star,
-                        size: 26,
-                        color: Color.fromARGB(255, 255, 191, 0),
-                      ),
-                      Icon(
-                        Icons.star,
-                        size: 26,
-                        color: Color.fromARGB(255, 255, 191, 0),
-                      ),
-                      Icon(
-                        Icons.star,
-                        size: 26,
-                        color: Color.fromARGB(255, 255, 191, 0),
-                      ),
-                    ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). The biological function of a flower is to facilitate reproduction, usually by providing a mechanism for the union of sperm with eggs. Flowers may facilitate outcrossing (fusion of sperm and eggs from different individuals in a population) resulting from cross-pollination or allow selfing (fusion of sperm and egg from the same flower) when self-pollination occurs.",
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
-                  const SizedBox(
-                    width: 66,
-                  ),
-                  Row(
-                    children: const [
-                      Icon(
-                        Icons.edit_location,
-                        size: 26,
-                        color: Color.fromARGB(168, 3, 65, 27),
-                        // color: Color.fromARGB(255, 186, 30, 30),
-                      ),
-                      SizedBox(
-                        width: 3,
-                      ),
-                      Text(
-                        "Flower Shop",
-                        style: TextStyle(fontSize: 19),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
+                ),
 
-            ],
+              ],
+            ),
           )),
     );
   }
